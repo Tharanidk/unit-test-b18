2cc66857b6ba28ec903ec7bdf6ec3e94c2c9617e
Tharani <tharani.dayadhi@gmail.com>
2021-10-07 04:35:50 +0530

    with tdd

--- tests/wordcount.test.js
+++ tests/wordcount.test.js
@@ -1,13 +1,10 @@
 var wordCount = require('../wordCount');
 
-test('returns word count 3', ()=>{
-    expect(wordCount.uniqueWordCount("Unit Test Example")).toBe(3);
-});
-
-test('returns word count 3', ()=>{
-    expect(wordCount.uniqueWordCount()).toBe(0);
-});
-
-test('returns word count 3', ()=>{
+test('returns 0 for empty strings', () => {
     expect(wordCount.uniqueWordCount('')).toBe(0);
+});
+
+
+test('returns 2 for "Batch 18"', () => {
+    expect(wordCount.uniqueWordCount('Batch 18')).toBe(2);
 });
