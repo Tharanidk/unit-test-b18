2ceaf5889c546d217f80b3e1858ab0055724d224
Tharani <tharani.dayadhi@gmail.com>
2021-10-07 04:14:19 +0530

    unit test cases

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,2 +1,15 @@
 var calculator = require('../calculator');
+//calculator imported. it will include all the functions
+
+test("adds 1 and 2 and return 3", () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("subtract 5 and 2 and return 3", () => {
+    expect(calculator.subtract(5, 2)).toBe(3);
+});
+
+test("multiply 1 and 2 and return 3", () => {
+    expect(calculator.multiply(1, 2)).toBe(2);
+});
 
